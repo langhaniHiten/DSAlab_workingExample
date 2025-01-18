36dd0e2e893eb08a8bf2c0e472622fad56517d77
Hiten kumar <hitenkumar8737@gmail.com>
2025-01-18 21:59:05 +0500

    Add files via upload

--- SumOFNaturalNumber.java
+++ SumOFNaturalNumber.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
-class SumOfNaturalNumbers{
-    public static void main(String[] args){
-        Scanner Scanner =new Scanner(System.in);
-        System.out.println("Enter a positive integer");
-        int n=Scanner.nextInt();
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            sum +=i;
+class SumOFNaturalNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter an number");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i * n);
         }
-        System.out.println("the sum of natural number is"+sum);
-
     }
 }
